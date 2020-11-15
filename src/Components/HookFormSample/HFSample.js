8eb92d1f613329510dd01940a6eb8f9551515367
YV <yagnavalk.c@gmail.com>
2020-11-16 00:40:57 +0530

    #

--- src/Components/HookFormSample/HFSample.js
+++ src/Components/HookFormSample/HFSample.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HFSample() {
     return (
         <React.Fragment>
-            <div>React Hook Form Sample1</div>
+            <div>React Hook Form Sample</div>
         </React.Fragment>
     )
 }
