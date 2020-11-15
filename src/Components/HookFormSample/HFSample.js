743371f7cc2c3d64fd008d32a6eec7dd6dc69b05
YV <yagnavalk.c@gmail.com>
2020-11-16 01:00:48 +0530

    Form validation working on click of Submit

--- src/Components/HookFormSample/HFSample.js
+++ src/Components/HookFormSample/HFSample.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function HFSample() {
-    return (
-        <React.Fragment>
-            <div>React Hook Form Sample</div>
-        </React.Fragment>
-    )
+  const { register, handleSubmit, errors } = useForm();
+
+  const onFormSubmit = (formData) => {
+    console.log(formData);
+  };
+  return (
+    <React.Fragment>
+      <form onSubmit={handleSubmit(onFormSubmit)}>
+        <input
+          type="text"
+          placeholder="FullName"
+          name="fullname"
+          ref={register({required:true, minLength:3})}
+        />
+        {errors.fullname && <div>Full Name Invalid</div>}
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          ref={register({ required: true })}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          ref={register({ required: "Password Required", minLength: {value:8, message:"Enter at least 8 chars"} })}
+        />
+        {errors.password && <div>{errors.password.message}</div> }
+        <input type="submit" />
+      </form>
+    </React.Fragment>
+  );
 }
